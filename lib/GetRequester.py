62ff057ab84da8a2edca178a1a8ab9621c1ae2b4
Immanuel <immanuelcherui1@gmail.com>
2024-04-12 20:31:50 +0300

    Done with assignments

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,22 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+
+        response = requests.get(self.url)
+        return response.content
+        
 
     def load_json(self):
-        pass
+
+        response_body = self.get_response_body()
+        json_data = json.loads(response_body)  # Convert response body to Python dictionary
+        return json_data
+
+        # response = requests.get(self.url)
+        # return response.json()
+        
+
+get_requester = GetRequester("https://github.com/immanuelcherui1/python-p4-getting-remote-data-lab.git")
+
+# get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+# print(get_requester.load_json())
